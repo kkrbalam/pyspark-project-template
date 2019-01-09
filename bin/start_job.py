e3f6f01ed0b344b75e4ed3434ccd8ce957b0bc7c
LinShauHsuan <cindyboy220@gmail.com>
2019-01-09 16:05:40 +0800

    [Initial] jupyter template

--- bin/start_job.py
+++ bin/start_job.py
@@ -1,18 +1,20 @@
-from __future__ import print_function
-
 import os
 import sys
+import re
 import traceback
+import pickle
 from datetime import timedelta
 
 import click
-from pyhocon import ConfigTree
+import papermill as pm
 
 import logging
-from cathay_configger import Configger
 from cathay_time_utils import TimeUtils
+from cathay_configger import Configger
 
-from project_template.job.submit_job import SubmitJob
+from cathay.setting.config import merge_env_configs
+from cathay.setting.jupyter import get_jupyter_dir, rm_jupyter_history, dump_configs
+
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -28,36 +30,39 @@ def receive(conf):
     logger.info("======== Start Submit Job ... ========")
     start = TimeUtils.get_now('ts')
 
-    # load configs
-    logger.info("Load configs...")
-    try:
-        confs = []
-        for sub_conf in conf.split(':'):
-            Configger.path(sub_conf)
-            confs.append(Configger.get_config())
-    except Exception:
-        logger.error("Start Submit Job fail, please check your configurations.")
-        exit(2)
-    else:
-        # merge configs
-        logger.info("Merge configs...")
-        configs = Configger.merge_configs(confs)
+    # get app home
+    app_home = os.environ['APP_HOME']
+
+    # merge all configs
+    configs = merge_env_configs(conf)
 
     try:
-        current_env = os.environ['ENV']
-        common_configs = configs.get('common', ConfigTree())
-        env_configs = configs.get(current_env)
-        merge_configs = Configger.merge_configs([common_configs, env_configs])
+        # rm old jupyter *.ipynb
+        rm_jupyter_history(configs, app_home)
+
+        # dump config to .pkl for jupyter to use
+        dump_configs(configs, app_home)
+
+        # get jupyter config
+        jupyter_path = get_jupyter_dir(configs, app_home)
+        jexec_file = configs.get('jupyter.job.exec_file')
+        joutput_prefix = configs.get('jupyter.job.output_prefix')
+
     except Exception:
         logger.error(traceback.format_exc())
-        logger.error("Start Submit Job fail, find config by {} ENV error.".format(current_env))
         exit(2)
 
-    # job
-    logger.info("Start submit job...")
+    # submit jupyter job
+    logger.info("Start submit jupyter job...")
     try:
-        submitJob = SubmitJob(config=merge_configs)
-        submitJob.start()
+        date = TimeUtils.cvt_datetime2str(TimeUtils.get_now('dt'), '%Y-%m-%d')
+        yyyymm = TimeUtils.cvt_datetime2str(TimeUtils.get_now('dt'), '%Y%m')
+        db_tbl = 'test'
+        pm.execute_notebook(
+            os.path.join(jupyter_path, jexec_file),
+            os.path.join(jupyter_path, '{}_{}.ipynb'.format(joutput_prefix, date)),
+            parameters=dict(yyyymm=yyyymm, db_tbl=db_tbl)
+        )
     except Exception:
         logger.error(traceback.format_exc())
         exit(2)
