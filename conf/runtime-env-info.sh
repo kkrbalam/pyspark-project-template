18f928faadfbb909b04b149f8562f225fe4ab7d5
LinShauHsuan <cindyboy220@gmail.com>
2019-01-09 09:35:33 +0800

    [Update] move SPARK_HOME path to the front of PATH

--- conf/runtime-env-info.sh
+++ conf/runtime-env-info.sh
@@ -14,26 +14,26 @@ if [[ "${ENV}" == "prod" ]]; then
     # == HADOOP + SPARK ==
     export HADOOP_CONF_DIR=/source/hadoop/conf
     export SPARK_HOME=/etc/spark-2.3.1-bin-hadoop2.6
-    export PATH=$PATH:$SPARK_HOME/bin
+    export PATH=$SPARK_HOME/bin:$PATH
 
 elif [[ "${ENV}" == "uat" ]]; then
     # == HADOOP + SPARK ==
     export HADOOP_CONF_DIR=/source/hadoop/conf
     # export SPARK_HOME=/etc/spark-2.3.1-bin-hadoop2.6
     export SPARK_HOME=/usr/local/spark-2.1.0-bin-hadoop2.7
-    export PATH=$PATH:$SPARK_HOME/bin
+    export PATH=$SPARK_HOME/bin:$PATH
 
 elif [[ "${ENV}" == "ut" ]]; then
     # == HADOOP + SPARK ==
     export HADOOP_CONF_DIR=/etc/hadoop/conf
     export SPARK_HOME=/opt/spark-2.1.0-bin-hadoop2.6
-    export PATH=$PATH:$SPARK_HOME/bin
+    export PATH=$SPARK_HOME/bin:$PATH
 
 elif [[ "${ENV}" == "dev" ]]; then
     # == HADOOP + SPARK ==
     export HADOOP_CONF_DIR=
     export SPARK_HOME=/usr/local/spark-2.1.0-bin-hadoop2.7
-    export PATH=$PATH:$SPARK_HOME/bin
+    export PATH=$SPARK_HOME/bin:$PATH
 
 else
 
